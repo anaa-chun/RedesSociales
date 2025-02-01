dbe78c21cad653516329e59dc2a5564934605eaa
anaa-chun <anachungdc@icloud.com>
2025-02-01 15:38:43 +0100

    modified:   RedesSociales.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 function inicial() {
-  let circulo = document.querySelectorAll("ul li a");
-
-  circulo.forEach((link) => {
-    link.addEventListener("mouseover", () => {
-      link.style.backgroundColor = "#000000";
+    let circulo = document.querySelectorAll("ul li a");
+  
+    circulo.forEach((link) => {
+      link.addEventListener("mouseover", () => {
+        link.style.backgroundColor = "#b3d9ff"; // Azul pastel
+      });
+  
+      link.addEventListener("mouseout", () => {
+        link.style.backgroundColor = ""; // Restablece al color original
+      });
     });
-
-    link.addEventListener("mouseout", () => {
-      link.style.backgroundColor = "";
-    });
+  }
+  
+  window.addEventListener("load", () => {
+    inicial();
   });
-}
-
-window.addEventListener("load", () => {
-  inicial();
-});
+  
