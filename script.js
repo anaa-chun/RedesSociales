f5de973ccc0b768983289b4c918aefa896b8224f
anaa-chun <anachungdc@icloud.com>
2025-02-01 14:05:04 +0100

    perfiles

---
+++ script.js
@@ -0,0 +1,17 @@
+function inicial() {
+  let circulo = document.querySelectorAll("ul li a");
+
+  circulo.forEach((link) => {
+    link.addEventListener("mouseover", () => {
+      link.style.backgroundColor = "#000000"; // Azul pastel
+    });
+
+    link.addEventListener("mouseout", () => {
+      link.style.backgroundColor = ""; // Restablece al color original
+    });
+  });
+}
+
+window.addEventListener("load", () => {
+  inicial();
+});
